c6f5a28a660be07be1db8969ffe2f48efe623f1e
ricky-kiva <ricky.kiva@gmail.com>
2023-05-12 02:38:14 +0700

    Software Design Principle summary

---
+++ Software Design Principle/1-software-design-principle.kt
@@ -0,0 +1,9 @@
+// Architecture is a pattern that define the form & structure for the overall software
+// Design is the interconnection between module & entity of software like packages, components, & classes
+
+// in software development, architecture is known as high-level, while design known as low-level
+
+// Software Design Principle is a guideline that could be used to avoid bad software design. It has 3 points that need to be avoided:
+// - Rigidity: condition where a system is hard to be changed or re-developed
+// - Fragility: condition where system tend to return wrong value when it's changed or re-developed
+// - Immobility: condition where system unable to reuse some component from another project
