c2d1b33609022a57e63762572debf4dc990e80cc
ricky-kiva <ricky.kiva@gmail.com>
2023-05-12 02:43:58 +0700

    SOLID introduction summary

---
+++ 4. SOLID/1-SOLID.kt
@@ -0,0 +1,6 @@
+// SOLID is a set of principle that made object-oriented designs more understandable, flexible, and maintainable. 
+// S: Single Responsibility
+// O: Open/Closed
+// L: Liskov Substitution
+// I: Interface Segregation
+// D: Dependencies Inversion
