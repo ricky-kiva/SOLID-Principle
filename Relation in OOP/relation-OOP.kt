b89aad3eed0ed6a90a3269a3256df23157858174
ricky-kiva <ricky.kiva@gmail.com>
2023-05-12 00:14:55 +0700

    OOP relation intro summary

---
+++ Relation in OOP/relation-OOP.kt
@@ -0,0 +1,4 @@
+// Object Relation defines how objects interact & collaborate towards each others. 3 Categories of object relation:
+// - Association: it has relation of "has-a". It state that a class has relation with another class
+// - Dependency: it explain that this class has dependency towards other class
+// - Generalization: it has relation of "is-a" from specific class to a more general class
